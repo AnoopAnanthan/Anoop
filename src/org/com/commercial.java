3b67fe3dabd872ae997a0fa3e3b6c9df0e52c364
AnoopAnanthan <anoopananthan9@gmail.com>
2022-08-16 22:20:10 +0530

    projec is changed

--- src/org/com/commercial.java
+++ src/org/com/commercial.java
@@ -2,4 +2,16 @@ package org.com;
 
 public class commercial {
 
+	
+	public void ABC() {
+		System.out.println("abc is a alphabet");
+	}
+
+	public void BCD() {
+		System.out.println("ABC is a alphabet");
+	}
+	public static void main(String[] args) {
+		
+		
+	}
 }
